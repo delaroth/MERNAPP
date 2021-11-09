edf7d00dec3e9090a2355e845c0b1b3738a7beb8
levi <levihalperin1998@gmail.com>
2021-11-09 18:31:00 +0200

    debugging

--- server/index.js
+++ server/index.js
@@ -6,6 +6,7 @@ import postRoutes from './routes/posts.js'
 
 
 const app = express()
+const dotenv = require("dotenv")
 
 
 app.use(express.json());
@@ -14,9 +15,9 @@ app.use(cors());
 // ```localhost:5000/posts```
 app.use('/posts', postRoutes)
 
+dotenv.config()
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>@cluster0.iupgl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
+const CONNECTION_URL = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@cluster0.iupgl.mongodb.net/cluster0?retryWrites=true&w=majority`
 const PORT = process.env.PORT || 5000;
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log('server running on port:' + PORT)))
